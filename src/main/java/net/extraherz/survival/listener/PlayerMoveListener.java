b7237e4aafd3ba9fe3bce9f0659eba3fb935a40b
xMonsterKing <raphael.krefeld2000@gmail.com>
2023-05-02 17:30:41 +0200

    add setting status query

--- src/main/java/net/extraherz/survival/listener/PlayerMoveListener.java
+++ src/main/java/net/extraherz/survival/listener/PlayerMoveListener.java
@@ -18,40 +18,42 @@ public class PlayerMoveListener implements Listener {
         Player player = event.getPlayer();
         Location location = player.getLocation();
 
-        switch (Utils.getPlayerGroup(player)) {
-            case "queen", "xi" -> {
-                if (event.getFrom().getX() != event.getTo().getX() || event.getFrom().getY() != event.getTo().getY() || event.getFrom().getZ() != event.getTo().getZ()) {
-                    player.getWorld().spawnParticle(Particle.REDSTONE, location, 3, 0, 0, 0.2D, new Particle.DustOptions(Color.fromRGB(168, 0, 0), 1));
+        if (Survival.getInstance().getSettingsDataYaml().particleState(player.getUniqueId())) {
+            switch (Utils.getPlayerGroup(player)) {
+                case "queen", "xi" -> {
+                    if (event.getFrom().getX() != event.getTo().getX() || event.getFrom().getY() != event.getTo().getY() || event.getFrom().getZ() != event.getTo().getZ()) {
+                        player.getWorld().spawnParticle(Particle.REDSTONE, location, 3, 0, 0, 0.2D, new Particle.DustOptions(Color.fromRGB(168, 0, 0), 1));
+                    }
                 }
-            }
-            case "administrator" -> {
-                if (event.getFrom().getX() != event.getTo().getX() || event.getFrom().getY() != event.getTo().getY() || event.getFrom().getZ() != event.getTo().getZ()) {
-                    player.getWorld().spawnParticle(Particle.REDSTONE, location, 3, 0, 0, 0.2D, new Particle.DustOptions(Color.fromRGB(251, 84, 84), 1));
+                case "administrator" -> {
+                    if (event.getFrom().getX() != event.getTo().getX() || event.getFrom().getY() != event.getTo().getY() || event.getFrom().getZ() != event.getTo().getZ()) {
+                        player.getWorld().spawnParticle(Particle.REDSTONE, location, 3, 0, 0, 0.2D, new Particle.DustOptions(Color.fromRGB(251, 84, 84), 1));
+                    }
                 }
-            }
-            case "management" -> {
-                if (event.getFrom().getX() != event.getTo().getX() || event.getFrom().getY() != event.getTo().getY() || event.getFrom().getZ() != event.getTo().getZ()) {
-                    player.getWorld().spawnParticle(Particle.REDSTONE, location, 3, 0, 0, 0.2D, new Particle.DustOptions(Color.fromRGB(251, 168, 0), 1));
+                case "management" -> {
+                    if (event.getFrom().getX() != event.getTo().getX() || event.getFrom().getY() != event.getTo().getY() || event.getFrom().getZ() != event.getTo().getZ()) {
+                        player.getWorld().spawnParticle(Particle.REDSTONE, location, 3, 0, 0, 0.2D, new Particle.DustOptions(Color.fromRGB(251, 168, 0), 1));
+                    }
                 }
-            }
-            case "mod" -> {
-                if (event.getFrom().getX() != event.getTo().getX() || event.getFrom().getY() != event.getTo().getY() || event.getFrom().getZ() != event.getTo().getZ()) {
-                    player.getWorld().spawnParticle(Particle.REDSTONE, location, 3, 0, 0, 0.2D, new Particle.DustOptions(Color.fromRGB(0, 0, 168), 1));
+                case "mod" -> {
+                    if (event.getFrom().getX() != event.getTo().getX() || event.getFrom().getY() != event.getTo().getY() || event.getFrom().getZ() != event.getTo().getZ()) {
+                        player.getWorld().spawnParticle(Particle.REDSTONE, location, 3, 0, 0, 0.2D, new Particle.DustOptions(Color.fromRGB(0, 0, 168), 1));
+                    }
                 }
-            }
-            case "developer" -> {
-                if (event.getFrom().getX() != event.getTo().getX() || event.getFrom().getY() != event.getTo().getY() || event.getFrom().getZ() != event.getTo().getZ()) {
-                    player.getWorld().spawnParticle(Particle.REDSTONE, location, 3, 0, 0, 0.2D, new Particle.DustOptions(Color.fromRGB(84, 251, 251), 1));
+                case "developer" -> {
+                    if (event.getFrom().getX() != event.getTo().getX() || event.getFrom().getY() != event.getTo().getY() || event.getFrom().getZ() != event.getTo().getZ()) {
+                        player.getWorld().spawnParticle(Particle.REDSTONE, location, 3, 0, 0, 0.2D, new Particle.DustOptions(Color.fromRGB(84, 251, 251), 1));
+                    }
                 }
-            }
-            case "helfer" -> {
-                if (event.getFrom().getX() != event.getTo().getX() || event.getFrom().getY() != event.getTo().getY() || event.getFrom().getZ() != event.getTo().getZ()) {
-                    player.getWorld().spawnParticle(Particle.REDSTONE, location, 3, 0, 0, 0.2D, new Particle.DustOptions(Color.fromRGB(84, 84, 251), 1));
+                case "helfer" -> {
+                    if (event.getFrom().getX() != event.getTo().getX() || event.getFrom().getY() != event.getTo().getY() || event.getFrom().getZ() != event.getTo().getZ()) {
+                        player.getWorld().spawnParticle(Particle.REDSTONE, location, 3, 0, 0, 0.2D, new Particle.DustOptions(Color.fromRGB(84, 84, 251), 1));
+                    }
                 }
-            }
-            case "booster" -> {
-                if (event.getFrom().getX() != event.getTo().getX() || event.getFrom().getY() != event.getTo().getY() || event.getFrom().getZ() != event.getTo().getZ()) {
-                    player.getWorld().spawnParticle(Particle.REDSTONE, location, 3, 0, 0, 0.2D, new Particle.DustOptions(Color.fromRGB(255, 85, 255), 1));
+                case "booster" -> {
+                    if (event.getFrom().getX() != event.getTo().getX() || event.getFrom().getY() != event.getTo().getY() || event.getFrom().getZ() != event.getTo().getZ()) {
+                        player.getWorld().spawnParticle(Particle.REDSTONE, location, 3, 0, 0, 0.2D, new Particle.DustOptions(Color.fromRGB(255, 85, 255), 1));
+                    }
                 }
             }
         }
